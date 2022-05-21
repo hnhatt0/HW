52d692987a50148ca66ce9be2a324a63b8753f20
hnhatt0 <104014967+hnhatt0@users.noreply.github.com>
2022-05-21 17:46:26 +0700

    12

--- Homework/hwb12a.js
+++ Homework/hwb12a.js
@@ -4,7 +4,10 @@ for(let i=0; i < listItem.length; i++) {
     let val = listItem[i];
     val.addEventListener('click', function(event) {
         const ele = event.target.parentNode;
-        ele.classList.add('active')
+        for(let i=0; i < listItem.length; i++) {
+            let val = listItem[i];
+            val.classList.remove('active');
+        }
+        ele.classList.add('active');
     })
-    //lam not
 }
